c797808641ec75762b8a88df4f58c5495b5a55c2
OtavioLhamas <otavio.m.lhamas@gmail.com>
2020-11-11 03:18:41 -0300

    - Installed VueSwal - Added a success/error notification when adding a movie

--- movie_rating_app/src/views/AddMovie.vue
+++ movie_rating_app/src/views/AddMovie.vue
@@ -15,9 +15,16 @@
     <v-select
       label="Movie Release Year"
       v-model="releaseYear"
+      required
+      :rules="releaseRules"
       :items="years"
     ></v-select>
-    <v-text-field label="Movie Genre" v-model="genre"></v-text-field>
+    <v-text-field
+      label="Movie Genre"
+      v-model="genre"
+      required
+      :rules="genreRules"
+    ></v-text-field>
     <v-btn @click="submit" :disabled="!valid"> submit </v-btn>
     <v-btn @click="clear">clear</v-btn>
   </v-form>
@@ -35,6 +42,11 @@ export default {
     genre: '',
     releaseYear: '',
     nameRules: [(v) => !!v || 'Movie name is required'],
+    genreRules: [
+      (v) => !!v || 'Movie genre is required',
+      (v) => (v && v.length <= 80) || 'Genre must be less than or equal to 80 characters.',
+    ],
+    releaseRules: [(v) => !!v || 'Movie release year is required'],
     select: null,
     years: ['2018', '2017', '2016', '2015'],
   }),
@@ -54,10 +66,23 @@ export default {
           headers: {
             'Content-Type': 'application/json',
           },
-        }).then(() => {
-          this.$router.push({ name: 'Home' });
-          this.$refs.form.reset();
-        }).catch(() => {});
+        })
+          .then(() => {
+            this.$swal(
+              'Great!',
+              'Movie added successfully!',
+              'success'
+            );
+            this.$router.push({ name: 'Home' });
+            this.$refs.form.reset();
+          })
+          .catch(() => {
+            this.$swal(
+              'Oh oo!',
+              'Could not add the movie!',
+              'error'
+            )
+          });
       }
       return true;
     },
