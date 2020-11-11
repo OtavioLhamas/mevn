dd6ce5a007e21e6f5ee1a6786b037762e128964b
OtavioLhamas <otavio.m.lhamas@gmail.com>
2020-11-10 21:46:43 -0300

    - Initial App side and top bar design

--- movie_rating_app/src/App.vue
+++ movie_rating_app/src/App.vue
@@ -1,60 +1,51 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app id="inspire">
+    <v-app-bar color="indigo" dark app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Home</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-navigation-drawer fixed v-model="drawer" app>
+      <v-list dense>
+        <router-link :to="{ name: 'Home' }" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-icon> mdi-home </v-icon>
+            </v-list-item-avatar>
+            <v-list-item-title> Home </v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <router-link :to="{ name: 'Contact' }" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-icon>mdi-card-account-details</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <v-container fluid>
+        <div id="app">
+          <router-view />
+        </div>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import './assets/stylesheets/main.css';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    drawer: true,
   }),
+  props: {
+    source: String,
+  },
 };
 </script>
